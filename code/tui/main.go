50e829461f0b72de60cbe70ee8171c7adcc9f367
brian <brianhsieh38@gmail.com>
2026-07-27 16:55:06 +1000

    hide expected klog error

--- code/tui/main.go
+++ code/tui/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"kube-thrifty/tui/internal/kube"
 	"kube-thrifty/tui/internal/tui"
 
 	tea "github.com/charmbracelet/bubbletea"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 )
 
 const (
@@ -19,6 +22,8 @@ const (
 )
 
 func main() {
+	configureKubeRuntimeErrors()
+
 	kubeconfigPath, err := kube.ResolveKubeconfigPath()
 	if err != nil {
 		exitWithMessage(err.Error())
@@ -45,4 +50,25 @@ func main() {
 func exitWithMessage(msg string) {
 	fmt.Fprintln(os.Stderr, "Kube-Thrifty:", msg)
 	os.Exit(1)
+}
+
+func configureKubeRuntimeErrors() {
+	utilruntime.ErrorHandlers = []utilruntime.ErrorHandler{
+		func(_ context.Context, err error, _ string, _ ...any) {
+			if err == nil || isExpectedPortForwardDisconnect(err) {
+				return
+			}
+		},
+	}
+}
+
+func isExpectedPortForwardDisconnect(err error) bool {
+	msg := strings.ToLower(err.Error())
+	if !strings.Contains(msg, "error copying from local connection to remote stream") {
+		return false
+	}
+
+	return strings.Contains(msg, "connection reset by peer") ||
+		strings.Contains(msg, "broken pipe") ||
+		strings.Contains(msg, "use of closed network connection")
 }
